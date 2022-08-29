0aa6c46ad6ea0ac22b1ac26da2ba99cda92acb55
Fernando Galvan <n.brighter00@gmail.com>
2022-08-29 15:44:51 -0500

    figuring small bug

--- JokesApp/Models/Joke.cs
+++ JokesApp/Models/Joke.cs
@@ -5,9 +5,9 @@ namespace JokesApp.Models
     public class Joke
     {
         public int Id { get; set; }
-        public int JokeQuestion { get; set; }
+        public  string JokeQuestion { get; set; }
 
-        public int JokeAnswer { get; set; }
+        public string JokeAnswer { get; set; }
 
         public Joke()
         {
