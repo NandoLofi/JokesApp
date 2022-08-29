94cb0e070b3d4d122354fe64a0ca5b28c04e3f31
Fernando Galvan <n.brighter00@gmail.com>
2022-08-29 11:30:47 -0500

    new controllers for mvc

---
+++ JokesApp/Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace JokesApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public int JokeQuestion { get; set; }
+
+        public int JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
